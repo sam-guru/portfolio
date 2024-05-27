711f37d3b9ebe55b9d0008e0106f1619e84c8c1a
Ikashi Ishieka <69310776+sam-guru@users.noreply.github.com>
2024-05-27 10:12:54 +0100

    change of default title

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -20,83 +20,47 @@ const Skills = () => {
         </h2>
 
         <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-<<<<<<< HEAD
             <img src={html} alt="html-logo" />
-=======
-            <img src={html} alt="html_logo" />
->>>>>>> 50a4f4a352eff60cefc41f6fd104b6d3205e1c8f
             <p className='mt-2'>HTML</p>
         </div>
 
         <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-<<<<<<< HEAD
             <img src={bootstrap} alt="bootstrap-logo" width={100} height={100}/>
-=======
-            <img src={bootstrap} alt="bootstrap_logo" width={100} height={100}/>
->>>>>>> 50a4f4a352eff60cefc41f6fd104b6d3205e1c8f
             <p className='mt-2'>Bootstrap</p>
         </div>
 
         <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-<<<<<<< HEAD
             <img src={tailwind} alt="tailwind-logo" width={100} height={100}/>
-=======
-            <img src={tailwind} alt="tailwind_logo" width={100} height={100}/>
->>>>>>> 50a4f4a352eff60cefc41f6fd104b6d3205e1c8f
             <p className='mt-2'>Tailwind</p>
         </div>
 
         <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-<<<<<<< HEAD
             <img src={javascript} alt="javascript-logo" width={100} height={100}/>
-=======
-            <img src={javascript} alt="javascript_logo" width={100} height={100}/>
->>>>>>> 50a4f4a352eff60cefc41f6fd104b6d3205e1c8f
             <p className='mt-2'>JavaScript</p>
         </div>
 
         <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-<<<<<<< HEAD
             <img src={react} alt="react-logo" width={100} height={100}/>
-=======
-            <img src={react} alt="react_logo" width={100} height={100}/>
->>>>>>> 50a4f4a352eff60cefc41f6fd104b6d3205e1c8f
             <p className='mt-2'>React</p>
         </div>
 
         <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-<<<<<<< HEAD
             <img src={java} alt="java-logo" width={100} height={100}/>
-=======
-            <img src={java} alt="java_logo" width={100} height={100}/>
->>>>>>> 50a4f4a352eff60cefc41f6fd104b6d3205e1c8f
             <p className='mt-2'>Java</p>
         </div>
         
         <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-<<<<<<< HEAD
             <img src={python} alt="python-logo" width={100} height={100}/>
-=======
-            <img src={python} alt="phthon_logo" width={100} height={100}/>
->>>>>>> 50a4f4a352eff60cefc41f6fd104b6d3205e1c8f
             <p className='mt-2'>Python</p>
         </div>
 
         <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-<<<<<<< HEAD
             <img src={php} alt="php-logo" width={100} height={100}/>
-=======
-            <img src={php} alt="php_logo" width={100} height={100}/>
->>>>>>> 50a4f4a352eff60cefc41f6fd104b6d3205e1c8f
             <p className='mt-2'>PHP</p>
         </div>
 
         <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-<<<<<<< HEAD
             <img src={Csharp} alt="C#-logo" width={100} height={100}/>
-=======
-            <img src={Csharp} alt="csharp_logo" width={100} height={100}/>
->>>>>>> 50a4f4a352eff60cefc41f6fd104b6d3205e1c8f
             <p className='mt-2'>CSharp</p>
         </div>
       
