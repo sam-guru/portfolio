aa8f6d7ef66219e90f9109558aa17f3bdeb6d5e3
Ikashi Ishieka <69310776+sam-guru@users.noreply.github.com>
2024-05-27 09:30:22 +0100

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -20,47 +20,47 @@ const Skills = () => {
         </h2>
 
         <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-            <img src={html} alt="" />
+            <img src={html} alt="html-logo" />
             <p className='mt-2'>HTML</p>
         </div>
 
         <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-            <img src={bootstrap} alt="" width={100} height={100}/>
+            <img src={bootstrap} alt="bootstrap-logo" width={100} height={100}/>
             <p className='mt-2'>Bootstrap</p>
         </div>
 
         <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-            <img src={tailwind} alt="" width={100} height={100}/>
+            <img src={tailwind} alt="tailwind-logo" width={100} height={100}/>
             <p className='mt-2'>Tailwind</p>
         </div>
 
         <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-            <img src={javascript} alt="" width={100} height={100}/>
+            <img src={javascript} alt="javascript-logo" width={100} height={100}/>
             <p className='mt-2'>JavaScript</p>
         </div>
 
         <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-            <img src={react} alt="" width={100} height={100}/>
+            <img src={react} alt="react-logo" width={100} height={100}/>
             <p className='mt-2'>React</p>
         </div>
 
         <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-            <img src={java} alt="" width={100} height={100}/>
+            <img src={java} alt="java-logo" width={100} height={100}/>
             <p className='mt-2'>Java</p>
         </div>
         
         <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-            <img src={python} alt="" width={100} height={100}/>
+            <img src={python} alt="phthon-logo" width={100} height={100}/>
             <p className='mt-2'>Python</p>
         </div>
 
         <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-            <img src={php} alt="" width={100} height={100}/>
+            <img src={php} alt="php-logo" width={100} height={100}/>
             <p className='mt-2'>PHP</p>
         </div>
 
         <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
-            <img src={Csharp} alt="" width={100} height={100}/>
+            <img src={Csharp} alt="csharp-logo" width={100} height={100}/>
             <p className='mt-2'>CSharp</p>
         </div>
       
