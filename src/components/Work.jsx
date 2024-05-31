329954331ef49318b63cf5b295d788d866091278
Ikashi Ishieka <69310776+sam-guru@users.noreply.github.com>
2024-05-31 01:04:26 +0100

    removed website description

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -61,7 +61,7 @@ const Work = () => {
                 <img src={proj5} alt="demo_logo"/>
                 <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex 
                                 flex-col justify-center items-center'>
-                    <span className='text-2xl font-bold text-white tracking-wider'>Migration Check</span>
+                    <span className='text-2xl font-bold text-white tracking-wider'>Portfolio</span>
                     <div className='pt-8 text-center'>
                         <a href="/">
                             <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700
