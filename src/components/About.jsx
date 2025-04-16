f7f4ca2fd8b7e237a5ff03088c15195020edbab1
Ikashi Ishieka <69310776+sam-guru@users.noreply.github.com>
2025-04-16 15:51:32 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
 
         <div className='mt-4 md:mt-0 text-left flex'>
             <div data-aos="fade-right" className='my-auto mx-6'>
-                <h2 className='text-4xl font-bold mb-4 primary-color'>About Me</h2>
+                <h2 className='text-4xl font-bold mb-4 primary-color'> About Me </h2>
                 <p className='text-base lg:text-lg'>
                   
                 My passion for technology drives me to constantly explore and develop bespoke applications that 
@@ -33,7 +33,7 @@ const About = () => {
                 frameworks, allowing me to deliver robust and scalable web applications. My 
                 ability to handle full-stack development projects ensures that I can contribute 
                 effectively to all stages of the development lifecycle, from design to 
-                deployment. <br /><br />
+                deployment. <br/><br/>
 
                 </p>
             </div>
