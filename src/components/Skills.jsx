2cda02fbe4b604926ad8489649822ed8a3efa60d
Ikashi Ishieka <samii.kashii@gmail.com>
2025-08-11 21:30:54 +0100

    skills padding adjusted

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -26,7 +26,7 @@ const Skills = () => {
     <div data-aos="zoom-in" data-aos-delay="200"  className='mt-20 dark:border dark:border-gray-600 bg-black text-gray-400 min-h-[150px] max-w-[1200px] mx-auto grid grid-rows-3 grid-flow-col
                         place-items-center md:flex md:justify-between md:items-center rounded-lg'>
 
-        <h2 className='row-span-3 text-blue-500 text-2xl md:tet-4xl font-bold m-4'>
+        <h2 className='row-span-3 text-blue-500 text-2xl md:tet-4xl font-bold m-4 mr-0'>
             My <br /> Tech <br /> Stack
         </h2>
 
@@ -74,17 +74,17 @@ const Skills = () => {
             <img src={Csharp} alt="C#-logo" width={100} height={100}/>
             <p className='mt-2'>CSharp</p>
         </div>
-        <div className='flex flex-col items-center m-4 mr-4 md:mr-0 sm:my-0 w-[40px] md:w-[100px]'>
+        <div className='flex flex-col items-center m-4 pr-4 md:pr-0 sm:my-0 w-[40px] md:w-[100px]'>
             <img src={nodejs} alt="C#-logo" width={100} height={100}/>
             <p className='mt-2'>NodeJS</p>
         </div>
 
-        <div className='flex flex-col items-center m-4 mr-4 md:mr-0 sm:my-0 w-[40px] md:w-[100px]'>
+        <div className='flex flex-col items-center m-4 pr-4 md:pr-0 sm:my-0 w-[40px] md:w-[100px]'>
             <img src={php} alt="php-logo" width={100} height={100}/>
             <p className='mt-2'>PHP</p>
         </div>
 
-         <div className='flex flex-col items-center m-4 mr-4 md:mr-0 sm:my-0 w-[40px] md:w-[100px]'>
+         <div className='flex flex-col items-center m-4 pr-4 md:mr-0 sm:my-0 w-[40px] md:w-[100px]'>
             <img src={PowerPlatform} alt="bootstrap-logo" width={100} height={100}/>
             <p className='mt-2 text-sm'>Power Platform</p>
         </div>
